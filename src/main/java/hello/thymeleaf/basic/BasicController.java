dba8f8ad8b3a0033c43da061a7df4cea0549b2de
jisue <tjdwltn121@gmail.com>
2023-01-05 18:57:42 +0900

    조건부 평가

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -122,5 +122,10 @@ public class BasicController {
         model.addAttribute("users",list);
     }
 
+    @GetMapping("/condition")
+    public String condition (Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
 
 }
